5022b9581a83d6785275354b1516ac470a9634b8
sukhjeetsingh <ssingh.0471@gmail.com>
2023-09-14 12:54:01 -0400

    new Multiples.java file

---
+++ src/Multiples.java
@@ -0,0 +1,18 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+        for (int i = 3; i < 1000; i++) {
+
+            // Similar to FizzBuzz, determine if i is divisible by 3 or 5.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
+
